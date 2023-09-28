637aa4981c59468840efa3c5335640e542584239
najeebib <najeebibraheem1999@hotmail.com>
2023-09-28 13:57:55 +0300

    implement scoreboard

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import time
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
-
-sceen = Screen()
-sceen.bgcolor("black")
-sceen.setup(height=600, width=800)
-sceen.title("pong")
-sceen.tracer(0)
+from scoreboard import  Scoreboard
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("pong")
+screen.tracer(0)
 
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 ball = Ball()
-sceen.listen()
+scoreboard = Scoreboard()
+screen.listen()
 
-sceen.onkey(r_paddle.go_up, "Up")
-sceen.onkey(r_paddle.go_down, "Down")
-sceen.onkey(l_paddle.go_up, "w")
-sceen.onkey(l_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
-    sceen.update()
+    screen.update()
     ball.move()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
@@ -30,5 +31,11 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320  or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
         ball.bounce_x()
 
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
-sceen.exitonclick()
+screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, position):
+    def __init__(self,position):
         super().__init__()
         self.shape("square")
         self.color("white")
@@ -11,9 +11,9 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
+        new_y = self.ycor() + 20;
         self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.ycor() - 20
+        new_y = self.ycor() - 20;
         self.goto(self.xcor(), new_y)
